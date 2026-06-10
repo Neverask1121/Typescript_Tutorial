c23850b5c2b4de57279883ef16c00ae31763250c
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-10 13:50:26 +0530

    Types completed

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+let message = 'Hello, World!';
+// create a new header 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add heading to the document
+document.body.appendChild(heading);
--- app.ts
+++ app.ts
@@ -1,2 +1,20 @@
-let message: string = 'Hello, World!';
-console.log(message);
+// let message: string = 'Hello, World!';
+
+// // create a new header 1 element
+
+// let heading = document.createElement('h1');
+// heading.textContent = message;
+
+// // add heading to the document
+
+// document.body.appendChild(heading);
+
+
+console.log('Hello'.length);
+console.log('Hello'.toLowerCase());
+
+
+console.log('Hello'.toUpperCase());
+
+
+const heading = document.querySelector('h1');
