19511c9eed46a20d931507b05ba774806766b4b8
Neverask1121 <rpsh2823@ramadevipublicschool.com>
2026-06-10 17:52:10 +0530

    Adding upto data types

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 "use strict";
-let message = 'Hello, World!';
-// create a new header 1 element
-let heading = document.createElement('h1');
-heading.textContent = message;
-// add heading to the document
-document.body.appendChild(heading);
+// // let message: string = 'Hello, World!';
+// // // create a new header 1 element
+// // let heading = document.createElement('h1');
+// // heading.textContent = message;
+// // // add heading to the document
+// // document.body.appendChild(heading);
+// // console.log('Hello'.length);
+// // console.log('Hello'.toLowerCase());
+// // console.log('Hello'.toUpperCase());
+// // const heading = document.querySelector('h1');
+// // syntax of the type annotation is like this 
+// // let counter: number = 1;
+// // console.log(counter);
+// //cannot assign one type to another
+// // assigning array of strings
+// let names: string[] = ['John', 'Jane', 'Peter', 'David', 'Mary'];
+// console.log(names);
+// //assigning array of objects
+// let person :{
+//   name: string;
+//   age: number;
+// }
+// person={
+//   name: 'John',
+//   age: 25,
+// };
+// console.log(person);
+// //function arguments and return types
+// let greeting : (name:string) => string;
+// greeting = function(name:string){
+//   return `Hi ${name}`; 
+// }
+// document.addEventListener('click', function (event) {
+//     console.log(event.button); 
+// });
+// the following shows some of the decimal numbers
+let counter = 0;
+let x = 100;
+let y = 100;
+console.log(counter);
--- app.ts
+++ app.ts
@@ -1,20 +1,71 @@
-// let message: string = 'Hello, World!';
+// // let message: string = 'Hello, World!';
 
-// // create a new header 1 element
+// // // create a new header 1 element
 
-// let heading = document.createElement('h1');
-// heading.textContent = message;
+// // let heading = document.createElement('h1');
+// // heading.textContent = message;
 
-// // add heading to the document
+// // // add heading to the document
 
-// document.body.appendChild(heading);
-
-
-console.log('Hello'.length);
-console.log('Hello'.toLowerCase());
+// // document.body.appendChild(heading);
 
 
-console.log('Hello'.toUpperCase());
+// // console.log('Hello'.length);
+// // console.log('Hello'.toLowerCase());
 
 
-const heading = document.querySelector('h1');
+// // console.log('Hello'.toUpperCase());
+
+
+// // const heading = document.querySelector('h1');
+
+
+// // syntax of the type annotation is like this 
+
+// // let counter: number = 1;
+// // console.log(counter);
+
+// //cannot assign one type to another
+
+// // assigning array of strings
+
+// let names: string[] = ['John', 'Jane', 'Peter', 'David', 'Mary'];
+// console.log(names);
+
+// //assigning array of objects
+
+
+// let person :{
+//   name: string;
+//   age: number;
+// }
+
+// person={
+//   name: 'John',
+//   age: 25,
+// };
+
+// console.log(person);
+
+
+// //function arguments and return types
+
+// let greeting : (name:string) => string;
+
+// greeting = function(name:string){
+//   return `Hi ${name}`; 
+// }
+
+// document.addEventListener('click', function (event) {
+//     console.log(event.button); 
+// });
+
+
+// the following shows some of the decimal numbers
+
+
+let counter: number = 0;
+let x: number = 100;
+let y: number = 100;
+let octal: number = 0o10;
+console.log(octal);
